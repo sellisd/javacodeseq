36db017ee8126fb9e2d9614ddf3a8ba3314be4fc
Diamantis Sellis <sellisd@gmail.com>
2024-12-19 01:01:07 +0100

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,15 @@ plugins {
 }
 
 group = "org.sellisd"
-version = "1.0.1"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("com.github.javaparser:javaparser-core:3.17.0")
-    implementation("info.picocli:picocli:4.5.2")
+    implementation("com.github.javaparser:javaparser-core:3.26.3")
+    implementation("info.picocli:picocli:4.7.6")
 }
 
 application{
